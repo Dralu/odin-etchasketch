bcbaba0f9dbc3aa1f1a0e6830e3d59f540d0da31
Dralu <louis@macbookair.home>
2026-01-20 22:41:33 +0100

    Add grid creating script

---
+++ script.js
@@ -0,0 +1,12 @@
+container = document.getElementById("container");
+for (i=0;i<16;i++){
+    const newCont=document.createElement("div");
+    for (j=0;j<16;j++){
+        const newDiv=document.createElement("div");
+        newDiv.textContent="TEST";
+        newDiv.id="box";
+        newCont.appendChild(newDiv);
+    }
+    newCont.id="line";
+    container.appendChild(newCont);
+}
