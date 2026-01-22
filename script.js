5dd7b194afd3ede553f757515c2f7d90bbb2eef9
Dralu <louis@macbookair.home>
2026-01-22 22:23:54 +0100

    Add hover effect

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ for (i=0;i<16;i++){
     const newCont=document.createElement("div");
     for (j=0;j<16;j++){
         const newDiv=document.createElement("div");
-        newDiv.textContent="TEST";
+        newDiv.textContent="";
         newDiv.id="box";
+        newDiv.addEventListener("mouseover",()=>{
+            newDiv.classList="hovered";
+        })
         newCont.appendChild(newDiv);
     }
     newCont.id="line";
