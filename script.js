437a03ada5acec013996da2d36c3b704c3cd9871
Dralu <louis@macbookair.home>
2026-02-08 22:28:49 +0100

    Add button for grid size

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 container = document.getElementById("container");
-for (i=0;i<16;i++){
-    const newCont=document.createElement("div");
-    for (j=0;j<16;j++){
-        const newDiv=document.createElement("div");
-        newDiv.textContent="";
-        newDiv.id="box";
-        newDiv.addEventListener("mouseover",()=>{
-            newDiv.classList="hovered";
-        })
-        newCont.appendChild(newDiv);
+function gridDrawing(sideNumber){
+    container.innerHTML="";
+    for (i=0;i<sideNumber;i++){
+
+        const newCont=document.createElement("div");
+        for (j=0;j<sideNumber;j++){
+            const newDiv=document.createElement("div");
+            newDiv.textContent="";
+            newDiv.id="box";
+            newDiv.addEventListener("mouseover",()=>{
+                newDiv.classList="hovered";
+            })
+            newCont.appendChild(newDiv);
+        }
+        newCont.id="line";
+        container.appendChild(newCont);
     }
-    newCont.id="line";
-    container.appendChild(newCont);
 }
+gridDrawing(25)
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click",()=>{
+    gridDrawing(prompt("What size?"))
+    }
+)
